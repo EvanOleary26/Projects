0727246ebf7a665995d942060f1eca9fcc6276a2
EvanOleary26 <43943154+EvanOleary26@users.noreply.github.com>
2022-11-30 16:08:22 -0500

    Add files via upload

---
+++ Tic Tac Toe.py
@@ -0,0 +1,42 @@
+
+
+map = ["-","-","-","-","-","-","-","-","-"]
+playerVal = [0,1]
+playerStr = ["Player 1","Player 2"]
+turn = 0
+rowmoves = ["L","M","B"]
+columnmoves = ["L","M","R"]
+
+print(f"{map[0]} | {map[1]} | {map[2]}\n"
+          "--+---+--\n"
+          f"{map[3]} | {map[4]} | {map[5]}\n"
+          "--+---+--\n"
+          f"{map[6]} | {map[7]} | {map[8]}")
+
+print("\n"
+      "Top row = T \n"
+       "Middle row = M \n" 
+       "Bottom row = B \n \n" 
+       "Left column = L \n"
+       "Middle column = M \n"
+       "Right column = R \n")
+print("an example entry is 'MR','MM','BR'")
+
+"""
+while True:
+
+    print(f"{PlayerStr[turn]} is up.")
+    move = input("Please input a location for your piece"))
+    while True:
+        
+        
+    
+    list(move.upper())
+
+    print(f"{map[0]} | {map[1]} | {map[2]}\n"
+          "--+---+--\n"
+          f"{map[3]} | {map[4]} | {map[5]}\n"
+          "--+---+--\n"
+          f"{map[6]} | {map[7]} | {map[8]}")
+
+"""
